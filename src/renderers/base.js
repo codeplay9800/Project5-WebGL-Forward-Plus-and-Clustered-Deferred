fd8a129bcfe5f4709f3031b0a5e4ef6ed07f2adb
Shubham Sharma <sharmashubham9800@gmail.com>
2021-11-01 16:30:02 -0400

    Code Refactored

--- src/renderers/base.js
+++ src/renderers/base.js
@@ -16,17 +16,6 @@ export default class BaseRenderer {
 
     updateClusters(camera, viewMatrix, scene) {
         // TODO: Update the cluster texture with the count and indices of the lights in each cluster
-        // This will take some time. The math is nontrivial...
-
-        // for (let z = 0; z < this._zSlices; ++z) {
-        //     for (let y = 0; y < this._ySlices; ++y) {
-        //         for (let x = 0; x < this._xSlices; ++x) {
-        //             let i = x + y * this._xSlices + z * this._xSlices * this._ySlices;
-        //             // Reset the light count to 0 for every cluster
-        //             this._clusterTexture.buffer[this._clusterTexture.bufferIndex(i, 0)] = 0;
-        //         }
-        //     }
-        // }
         var yFOVAngle = Math.tan((camera.fov / 2) * (180 / Math.PI));
         var xFOVAngle = camera.aspect * yFOVAngle;
         // for each light in the scene
@@ -51,49 +40,45 @@ export default class BaseRenderer {
             let maxXYZ = vec4.create(lightPos);
             vec4.add(maxXYZ, lightPos, lightRad);
 
-            // 1.a) convert bounding box to camera space
+            // getting bounding box to camera space
             vec4.transformMat4(minXYZ, minXYZ, viewMatrix);
             vec4.transformMat4(maxXYZ, maxXYZ, viewMatrix);
 
             minXYZ[2] *= -1;
             maxXYZ[2] *= -1;
 
-            // 2) determine which cluster light is in
-
-            // 2.a) compute min and max of x slice
+            //determine which cluster light is in
             let xSliceHalfWidth = xFOVAngle * lightPos[2];
             let xMin = Math.floor(minXYZ[0] + xSliceHalfWidth / (2 * xSliceHalfWidth / this._xSlices));
             let xMax = Math.ceil(maxXYZ[0] + xSliceHalfWidth / (2 * xSliceHalfWidth / this._xSlices));
             xMin = Math.min(Math.max(xMin, 0), this._xSlices - 1);
             xMax = Math.min(Math.max(xMax, 0), this._xSlices - 1);
 
-            // 2.b) compute min and max of y slice 
             let ySliceHalfWidth = yFOVAngle * lightPos[2];
             let yMin = Math.floor(minXYZ[1] + ySliceHalfWidth / (2 * ySliceHalfWidth / this._ySlices));
             let yMax = Math.ceil(maxXYZ[1] + ySliceHalfWidth / (2 * ySliceHalfWidth / this._ySlices));
             yMin = Math.min(Math.max(yMin, 0), this._ySlices - 1);
             yMax = Math.min(Math.max(yMax, 0), this._ySlices - 1);
 
-            // 2.c) compute min and max of z slice 
             let zSlice = (camera.far - camera.near) / this._zSlices;
             let zMin = Math.floor(minXYZ[2] / zSlice);
             let zMax = Math.ceil(maxXYZ[2] / zSlice);
             zMin = Math.min(Math.max(zMin, 0), this._zSlices - 1);
             zMax = Math.min(Math.max(zMax, 0), this._zSlices - 1);
 
-            // 3) increment light count for cluster and add index to cluster
+            //Increment light count and add index to cluster
             for (let z = zMin; z <= zMax; ++z) {
                 for (let y = yMin; y <= yMax; ++y) {
                     for (let x = xMin; x <= xMax; ++x) {
                         let idx = x + y * this._xSlices + z * this._xSlices * this._ySlices;
-                        let numLightsInCluster = this._clusterTexture.buffer[this._clusterTexture.bufferIndex(idx, 0)] + 1;
+                        let numLInCluster = this._clusterTexture.buffer[this._clusterTexture.bufferIndex(idx, 0)] + 1;
 
-                        if (numLightsInCluster > MAX_LIGHTS_PER_CLUSTER) continue;
+                        if (numLInCluster > MAX_LIGHTS_PER_CLUSTER) continue;
 
-                        let elemNum = Math.floor(numLightsInCluster / 4);
-                        let elemIdx = Math.floor(numLightsInCluster % 4);
+                        let elemNum = Math.floor(numLInCluster / 4);
+                        let elemIdx = Math.floor(numLInCluster % 4);
 
-                        this._clusterTexture.buffer[this._clusterTexture.bufferIndex(idx, 0)] = numLightsInCluster;
+                        this._clusterTexture.buffer[this._clusterTexture.bufferIndex(idx, 0)] = numLInCluster;
                         this._clusterTexture.buffer[this._clusterTexture.bufferIndex(idx, elemNum) + elemIdx] = i;
                     }
                 }
